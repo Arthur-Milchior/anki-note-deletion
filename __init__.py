bbdb6fdad9da5accc2b946167fb84f118b5ffa8f
Arthur-Milchior <arthur@milchior.fr>
2019-03-24 22:35:50 +0100

    Correctiong #1

--- __init__.py
+++ __init__.py
@@ -4,11 +4,4 @@
 # License: GNU GPL, version 3 or later; http://www.gnu.org/copyleft/gpl.html
 # Feel free to contribute to this code on https://github.com/Arthur-Milchior/anki-note-deletion
 # Add-on number 12287769 https://ankiweb.net/shared/info/12287769
-import sys
-if (sys.version_info > (3, 0)):
-     # Python 3 code in this block
-    from . import explainDeletion21
-else:
-     # Python 2 code in this block
-    from . import explainDeletion20
-
+from . import cards, syncer, notes, models, decks, integrity
